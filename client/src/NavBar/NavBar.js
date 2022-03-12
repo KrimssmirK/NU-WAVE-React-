5f00fb5d5ad109bda950dad97e4cfc61efb903c9
KrimssmirK <72426749+KrimssmirK@users.noreply.github.com>
2022-03-13 04:32:25 +0900

    added NavBar

---
+++ client/src/NavBar/NavBar.js
@@ -0,0 +1,105 @@
+import { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import './NavBar.css'
+import logo from './img/logo.png'
+
+class NavBar extends Component {
+  
+  handleClickSignInSignUpCart = () => {
+    alert('The feature will be available soon')
+  }
+  
+  render() {
+    const cartColor = { color: '#35408E' }
+    return (
+      <div className ="container-fluid">
+        <nav className = "navbar navbar-expand-lg navbar-dark py-0">
+          <a href = "/index.html"  className ="navbar-brand me-0">
+            <div className='container'>
+              <div className='row'>
+                <div className='col-3 ps-0'>
+                  <div className='n-logo'>
+                    <img src={logo} alt='logo' />
+                  </div>
+                </div>
+                <div className='col-8 nu-title px-md-0 pt-2'>
+                  <div className='nav-title fs-6 '>NU WAVE<br/></div>
+                  <div className='nav-sub fs-6'>Creative &#38; Technology Hub</div>
+                </div>
+              </div>
+            </div>              
+          </a>  
+          <button 
+            className="navbar navbar-toggler navbar-dark" 
+            type="button" 
+            data-bs-toggle="collapse" 
+            data-bs-target="#toggleMobileMenu" 
+            aria-controls="toggleMobileMenu" 
+            aria-expanded="false" 
+            aria-label="Toggle Navigation">
+              <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className = "collapse navbar-collapse" id = "toggleMobileMenu">
+            <div className='container'>
+              <div className='row'>
+                <div className='col-md-9 px-0 mx-0'>
+                  <div className='d-lg-flex justify-content-center'>
+                    <ul className = "navbar-nav">
+                      <li>
+                        <a className = "nav-link text-white mx-3" href = "./index.html">Home</a>
+                      </li>
+                      <li>
+                        <a className = "nav-link text-white mx-3" href = "./exhibits.html">Exhibits</a>
+                      </li>
+                      <li>
+                        <a className = "nav-link text-white mx-3" href = "./gallery.html">Gallery</a>
+                      </li>
+                      <li>
+                        <a className = "nav-link text-white mx-3" href = "./about.html">About</a>
+                      </li>
+                      <li>
+                        <a className = "nav-link text-white mx-3" href = "./contact.html">Contact</a>
+                      </li>
+                    </ul>
+                  </div> 
+                </div>
+                <div className='col-md-3 px-0 mx-0'>
+                  <div className='float-md-end'>
+                    <ul className = "navbar-nav mx-3" onClick={this.handleClickSignInSignUpCart}>
+                      <div className='vl'>
+                        <li
+                          data-bs-toggle="tooltip"
+                          data-bs-placement="top"
+                          title= "Disabled for now">
+                          <a className = "nav-link text-white disabled" href = "">SIGN UP</a>
+                        </li>
+                      </div>
+                      <li
+                        data-bs-toggle="tooltip"
+                        data-bs-placement="top"
+                        title= "Disabled for now">
+                        <a className = "nav-link text-white disabled" href = "">SIGN IN</a>
+                      </li>
+                      <li
+                        data-bs-toggle="tooltip"
+                        data-bs-placement="top"
+                        title= "Disabled for now">
+                        <a className = "nav-link text-white disabled" href = "">
+                        <i className="bi bi-cart3 b-cart-icon" style={cartColor}></i>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div> 
+          </div>
+        </nav>
+      </div>
+    )
+  }  
+}
+
+export default NavBar

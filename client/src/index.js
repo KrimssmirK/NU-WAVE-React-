dec965d0d84b3d35e53aec510f73e8fe0c66dd78
KrimssmirK <72426749+KrimssmirK@users.noreply.github.com>
2022-03-26 03:10:41 +0900

    added bootstrap 5

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,10 @@ import './index.css'
 import { BrowserRouter as Router } from "react-router-dom"
 import App from './App'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
 ReactDOM.render(
   <Router>
     <App />

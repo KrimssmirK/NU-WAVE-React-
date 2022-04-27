c62e6e6624d126517b42b25a9ea594867af54e8a
KrimssmirK <72426749+KrimssmirK@users.noreply.github.com>
2022-04-28 01:29:31 +0900

    added new Footer

---
+++ client/src/Footer/components/Contact.jsx
@@ -0,0 +1,17 @@
+const Contact = () => (
+  <div className='Contact'>
+    <div className='title'>
+      <h1 className='h5'>Contact Us</h1>
+    </div>
+    <div className='content'>
+      <p>
+        National University<br/>
+        Center for Innovation and Entrepreneurship<br/>
+        551 M.F. Jhocson St. Sampaloc, Manila, PH 1008
+      </p>
+    </div>
+    <i className="bi bi-telephone-fill" style={{ color: 'black' }}></i><span> +632 8712-1900</span>
+  </div> 
+)
+
+export default Contact
---
+++ client/src/Footer/components/QuickLinks.jsx
@@ -0,0 +1,45 @@
+import { Link } from 'react-router-dom'
+
+const QuickLinks = () => (
+    <div className='QuickLinks'>
+      <div className='title'>
+        <h1 className='h5'>Quick Links</h1>
+      </div>
+      <div className='content'>
+        <ul style={{ listStyleType: 'none', padding: 0}}>
+          <li >
+            <Link to='#' style={{ color: 'black', textDecoration: 'none'}}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to='#' style={{ color: 'black', textDecoration: 'none'}}>
+              Products
+            </Link>
+          </li>
+          <li>
+            <Link to='#' style={{ color: 'black', textDecoration: 'none'}}>
+              Exhibits
+            </Link>
+          </li>
+          <li>
+            <Link to='#' style={{ color: 'black', textDecoration: 'none'}}>
+              Innovators
+            </Link>
+          </li>
+          <li>
+            <Link to='#' style={{ color: 'black', textDecoration: 'none'}}>
+              Articles
+            </Link>
+          </li>
+          <li>
+            <Link to='#' style={{ color: 'black', textDecoration: 'none'}}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div> 
+  )
+  
+  export default QuickLinks

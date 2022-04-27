3b96564e392107e6afdf278a732d629692901752
KrimssmirK <72426749+KrimssmirK@users.noreply.github.com>
2022-04-27 14:03:14 +0900

    i changed the navbar

--- client/src/NavBar/NavBar.jsx
+++ client/src/NavBar/NavBar.jsx
@@ -1,6 +1,6 @@
 import { Component } from 'react'
 import './NavBar.css'
-// import logo from './img/logo.png'
+import logo from './img/logo.png'
 import { NavLink, Link } from 'react-router-dom'
 import account_bg from './img/account-bg.png';
 import wave from './img/New_Wave.svg'
@@ -15,33 +15,23 @@ class NavBar extends Component {
     return (
       <div className="Navbar container-fluid px-0">
         <div className="account row">
-          <div className="col col-sm-9">
-            <img src={account_bg} alt="wave" className="d-none d-sm-block w-100"/>
-          </div>
-          <div className="col col-sm-3 justify-content-end mx-0">
-            <div className="signIn px-4 py-1">
-              <button className="btn rounded-pill border-white text-light">Sign up</button>
-              <Link to="#" className="ms-3 px-3 border-start text-decoration-none text-light">Sign in</Link>
-            </div>
-          </div>
-        </div>
-          <div className="container py-3">
-            <div className="logo">
-            </div>
-          </div>
-          
-          <nav className="navbar navbar-expand-md navbar-dark">
+        
+        <nav className="navbar navbar-expand-md navbar-dark" style={{ backgroundColor: 'white'}}>
             <div className="container-fluid">
               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
               </button>
-              <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
+              <div className="collapse navbar-collapse" id="navbarNav">
+                <div className='wave-logo'>
+                  <img src={logo} alt='logo'/>
+                </div>
+             
                 <ul className="navbar-nav">
                   <li className="nav-item">
                     <NavLink className={({isActive}) => (isActive ? "active" : "nav-link")} aria-current="page" to="/">Home</NavLink>
                   </li>
                   <li className="nav-item">
-                    <NavLink className={({isActive}) => (isActive ? "active" : "nav-link")} to="/exhibit">Exhibit</NavLink>
+                    <NavLink className={({isActive}) => (isActive ? "active" : "nav-link")} to="/exhibits">Exhibit</NavLink>
                   </li>
                   <li className="nav-item">
                     <NavLink className={({isActive}) => (isActive ? "active" : "nav-link")} to="/gallery">Gallery</NavLink>
@@ -49,17 +39,39 @@ class NavBar extends Component {
                   <li className="nav-item">
                     <NavLink className={({isActive}) => (isActive ? "active" : "nav-link")}to="/article">Article</NavLink>
                   </li>
-                  <li className="nav-item">
+                  {/* <li className="nav-item">
                     <NavLink className={({isActive}) => (isActive ? "active" : "nav-link")} to="/about">About</NavLink>
                   </li>
                   <li className="nav-item">
                     <NavLink className={({isActive}) => (isActive ? "active" : "nav-link")}to="/contact">Contact</NavLink>
-                  </li>
+                  </li> */}
                 </ul>
+                   <div className="signIn px-4 py-1" style={{color: 'black', zIndex: 1}}>
+                      <button className="btn rounded-pill border-white text-light" style={{color: 'black', zIndex: 1}}>Sign up</button>
+                      <Link to="#" className="ms-3 px-3 border-start text-decoration-none text-light" style={{color: 'black', zIndex: 1}}>Sign in</Link>
+                    </div>
               </div>
             </div>
           </nav>
-          <img src={wave} alt="wave" className="w-100 d-none d-sm-block"/>
+          {/* <div className="col col-sm-9">
+            <img src={account_bg} alt="wave" className="d-none d-sm-block w-100"/>
+          </div> */}
+          <div className="col col-sm-3 justify-content-end mx-0">
+            {/* <div className="signIn px-4 py-1">
+              <button className="btn rounded-pill border-white text-light">Sign up</button>
+              <Link to="#" className="ms-3 px-3 border-start text-decoration-none text-light">Sign in</Link>
+            </div> */}
+          </div>
+        </div>
+
+
+          <div className="container py-3">
+            <div className="logo">
+            </div>
+          </div>
+          
+         
+          {/* <img src={wave} alt="wave" className="w-100 d-none d-sm-block"/> */}
 
         
 
